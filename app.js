9be41f6f7472725980aea3b8ea705312fe546b7c
ConstanceEDJ <constance.daire@sciencespo.fr>
2018-03-05 16:09:37 +0100

    debug chrome

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ $(document).ready(function() {
   var controller = new ScrollMagic.Controller();
 
   var wipeAnimation = new TimelineMax()
-    .fromTo('section#chapitre-1', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From botton
-    .fromTo('section#chapitre-2', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone }) // From right
-    .fromTo('section#chapitre-3', 1, { y: '-100%' }, { y: '0%', ease: Linear.easeNone }) // From top
-    .fromTo('section#chapitre-4', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone }) // From left
-    .fromTo('section#chapitre-5', 1, { x: '-100%' }, { x: '0%', ease: Linear.easeNone })
+    .fromTo('section#chapitre-1', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From bottom
+    .fromTo('section#chapitre-2', 1, { y: '100%' }, { y: '0%', ease: Linear.easeNone }) // From right
+    .fromTo('section#chapitre-3', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
+    .fromTo('section#chapitre-4', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
+    .fromTo('section#chapitre-5', 1, { x: '100%' }, { x: '0%', ease: Linear.easeNone })// From right
 
 
   // create a scene
@@ -20,12 +20,27 @@ $(document).ready(function() {
     .setTween(wipeAnimation)
     .on("update", function (scene) {
       const progress = scene.target.progress()
-      if (progress < 0.16) {
-        $('#intro video')[0].play()
+       if (progress < 0.16) {
+        startVideo('intro')
       } else {
-        $('#intro video')[0].pause()
+        stopVideo('intro')
+      }
+      if (progress > 0.50 && progress < 0.66) {
+        startVideo('chapitre-3')
+      } else {
+        stopVideo('chapitre-3')
       }
     })
     .addIndicators()
     .addTo(controller);
 });
+
+function startVideo(chapitre) {
+  $('#'+chapitre+' video').addClass('active')
+  $('#'+chapitre+' video')[0].play()
+}
+
+function stopVideo(chapitre) {
+  $('#'+chapitre+' video').removeClass('active')
+  $('#'+chapitre+' video')[0].pause()
+}
